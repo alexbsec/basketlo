acd9409f90badbdecd0a30ba199ca28a2ecb614f
alexbsec <j.a.c.buschinelli@gmail.com>
2025-01-01 15:22:32 -0300

    added movement but something is wrong (buffer overflow when closing window)

--- src/game.cc
+++ src/game.cc
@@ -1,5 +1,6 @@
 #include "include/game.hpp"
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/VideoMode.hpp>
 
 const unsigned int WIDTH = 800;
@@ -8,7 +9,11 @@ const float RADIUS = 10.0f;
 
 Game::Game(const string& title)
     : _mWindow(sf::RenderWindow(sf::VideoMode(WIDTH, HEIGHT), title))
-    , _mPlayer(sf::CircleShape(RADIUS)) {}
+    , _mPlayer(sf::CircleShape(RADIUS))
+    , _mIsMovingUp(false)
+    , _mIsMovingDown(false)
+    , _mIsMovingLeft(false)
+    , _mIsMovingRight(false) {}
     
 void Game::Run() {
     while (_mWindow.isOpen()) {
@@ -21,13 +26,49 @@ void Game::Run() {
 void Game::ProcessEvents() {
     sf::Event event;
     while (_mWindow.pollEvent(event)) {
-        if (event.type == sf::Event::Closed) {
-            _mWindow.close();
+        switch (event.type) {
+            case sf::Event::KeyPressed: {
+                HandlePlayerInput(event.key.code, true);
+                break;
+            } 
+
+            case sf::Event::KeyReleased: {
+                HandlePlayerInput(event.key.code, false);
+                break;
+            }
+
+            case sf::Event::Closed: {
+                _mWindow.close();
+                break;
+            }
+
+            default:
+                break;
         }
     }
 }
 
-void Game::Update() {}
+void Game::Update() {
+    sf::Vector2f movementVector(0.0f, 0.0f);
+    
+    if (_mIsMovingUp) {
+        movementVector.y -= 1.0f;
+    }
+
+    if (_mIsMovingDown) {
+        movementVector.y += 1.0f;
+    }
+
+    if (_mIsMovingLeft) {
+        movementVector.x -= 1.0f;
+    }
+
+    if (_mIsMovingRight) {
+        movementVector.x += 1.0f;
+    }
+
+    _mPlayer.move(movementVector);
+}
 
 void Game::Render() {
     _mWindow.clear();
@@ -35,4 +76,20 @@ void Game::Render() {
     _mWindow.display();
 }
 
+void Game::HandlePlayerInput(sf::Keyboard::Key keyCode, bool isPressed) {
+    if (keyCode == sf::Keyboard::Key::W) {
+        _mIsMovingUp = isPressed; 
+    }
 
+    if (keyCode == sf::Keyboard::Key::S) {
+        _mIsMovingDown = isPressed;
+    }
+
+    if (keyCode == sf::Keyboard::Key::A) {
+        _mIsMovingLeft = isPressed;
+    }
+
+    if (keyCode == sf::Keyboard::Key::D) {
+        _mIsMovingRight = isPressed;
+    }
+}
--- src/include/game.hpp
+++ src/include/game.hpp
@@ -2,7 +2,19 @@
 #define _BASKTLO_GAME_HPP
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include <string>
+#include <unordered_map>
+
+template <typename T, typename Q>
+using umap = std::unordered_map<T, Q>;
+
+enum class Moving {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
 
 using string = std::string;
 
@@ -15,9 +27,14 @@ class Game {
         void ProcessEvents();
         void Update();
         void Render();
+        void HandlePlayerInput(sf::Keyboard::Key keyCode, bool isPressed);
 
         sf::RenderWindow _mWindow;
         sf::CircleShape _mPlayer;
+        bool _mIsMovingUp;
+        bool _mIsMovingDown;
+        bool _mIsMovingLeft;
+        bool _mIsMovingRight;
 };
 
 #endif
