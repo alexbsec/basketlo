af46dfc895773bce75936b15c5f5c64c031ebb1a
alexbsec <j.a.c.buschinelli@gmail.com>
2025-01-03 10:04:08 -0300

    final for overriden method

--- src/include/scene_node.hpp
+++ src/include/scene_node.hpp
@@ -1,31 +1,32 @@
 #ifndef _BASKTLO_SCENE_NODE_HPP
 #define _BASKTLO_SCENE_NODE_HPP
 
+#include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/RenderStates.hpp>
 #include <SFML/Graphics/Transformable.hpp>
-#include <SFML/Graphics/Drawable.hpp>
 #include <SFML/System/NonCopyable.hpp>
 #include <memory>
 #include <vector>
 
 template <typename T> using vector = std::vector<T>;
 
-class SceneNode : public sf::Transformable, public sf::Drawable,
+class SceneNode : public sf::Transformable,
+                  public sf::Drawable,
                   private sf::NonCopyable {
 public:
   typedef std::unique_ptr<SceneNode> Ptr;
   SceneNode();
   void AttachChild(Ptr child);
-  Ptr DetachChild(const SceneNode& node);
+  Ptr DetachChild(const SceneNode &node);
 
 private:
-  virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
-    final;
-  virtual void DrawCurrent(sf::RenderTarget& target, sf::RenderStates states)
-    const;
+  virtual void draw(sf::RenderTarget &target,
+                    sf::RenderStates states) const final;
+  virtual void DrawCurrent(sf::RenderTarget &target,
+                           sf::RenderStates states) const;
 
   vector<Ptr> _mChildren;
-  SceneNode* _mParent;
+  SceneNode *_mParent;
 };
 
 #endif
